fc9b985cc76c59836039719576ce3326f665d361
ettienekorayyi <corralstephenmelben@gmail.com>
2024-11-27 12:53:55 +1100

    Replaced API endpoint

--- src/api/taskstechApi.js
+++ src/api/taskstechApi.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 // need to add username and password
 const taskstechApi = axios.create({
-    baseURL: "http://localhost:5047/api/",
-    // https://engramar.pythonanywhere.com/api/v1/ 
-    // https://apiwrklg.azurewebsites.net/api/
+    baseURL: "https://worklogapi.fly.dev/api/",
     // http://localhost:5047/api/
+    // https://worklogapi.fly.dev/api/jobstatus
 });
 
 export default taskstechApi;
